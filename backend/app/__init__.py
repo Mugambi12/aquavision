e49130ab81cb55dfad738c98e33c6f35fdec4b24
mugambi12 <silasmungiria16@gmail.com>
2024-06-11 22:48:52 +0300

    refactor: Update Flask app initialization and routing
    The Flask app initialization and routing have been updated to improve code organization and maintainability. The `__init__.py` file has been modified to include the necessary imports and create the Flask app instance. The `main.py` file has been removed as it is no longer needed. The `run.py` file has been updated to import the `create_app` function from the `app` module. This refactoring enhances the structure of the Flask app and simplifies the codebase.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,17 +1,38 @@
-#from flask import Flask
-#from flask_mongoengine import MongoEngine
-#from flask_restx import Api
-#from app.config.default import Config
-#
-#db = MongoEngine()
-#
-#def create_app(config_class=Config):
-#    app = Flask(__name__)
-#    app.config.from_object(config_class)
-#    db.init_app(app)
-#
-#    from app.resources.user import user_ns
-#    api = Api(app)
-#    api.add_namespace(user_ns)
-#
-#    return app
+# main
+from flask import Flask
+from flask_restx import Resource
+
+from .config.development import DevelopmentConfig
+from .extensions.database import db
+from .extensions.api import api
+
+from .models.users import User
+from .models.settings import Settings
+
+from .resources.user import api as user_api
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
+    db.init_app(app)
+    api.init_app(app, doc='/doc', title='API', version='1.0', description='API Documentation')
+    
+    api.add_namespace(user_api, path='/users')
+
+    @api.route('/hello')
+    class HelloWorld(Resource):
+        def get(self):
+            return {'hello': 'world'}
+
+    @app.shell_context_processor
+    def make_shell_context():
+        return {
+            'db': db,
+            'User': User,
+            'Settings': Settings
+        }
+
+    with app.app_context():
+        db.create_all()
+
+    return app
