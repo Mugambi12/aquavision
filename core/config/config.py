f67c4cde8f62e17a42ffe2722a801174cd02ac36
mugambi12 <silasmungiria16@gmail.com>
2024-06-20 01:23:31 +0300

    chore: Update database configuration and remove unnecessary code

--- core/config/config.py
+++ core/config/config.py
@@ -6,6 +6,34 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
+class Config:
+    SECRET_KEY = config('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CSRF_SESSION_KEY = config('CSRF_SESSION_KEY')
+    PORT = config('PORT')
+    HOST = config('HOST')
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'dev.sqlite3')
+    SQLALCHEMY_ECHO = True
+    SESSION_COOKIE_SECURE = False
+    PERMANENT_SESSION_LIFETIME = timedelta(hours=1.5)
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI')
+    DEBUG = False
+    SQLALCHEMY_ECHO = config('SQLALCHEMY_ECHO', cast=bool)
+    SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE', cast=bool)
+    PERMANENT_SESSION_LIFETIME = timedelta(hours=1.5)
+
+class TestConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'test.sqlite3')
+    SQLALCHEMY_ECHO = False
+    SESSION_COOKIE_SECURE = False
+    PERMANENT_SESSION_LIFETIME = timedelta(hours=1.5)
+
 class MailConfig:
     MAIL_SERVER = config('MAIL_SERVER', default='smtp.gmail.com')
     MAIL_PORT = config('MAIL_PORT', default=465, cast=int)
@@ -13,41 +41,3 @@ class MailConfig:
     MAIL_PASSWORD = config('MAIL_PASSWORD')
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-
-    MAIL_SUPPRESS_SEND = False
-    MAIL_DEBUG = False
-    MAIL_FAIL_SILENTLY = False
-
-
-class Config:
-    SECRET_KEY = config('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool)
-    PORT = config('PORT')
-    HOST = config('HOST')
-
-    CSRF_ENABLED = config('CSRF_ENABLED', cast=bool)
-    CSRF_SESSION_KEY = config('CSRF_SESSION_KEY')
-    WTF_CSRF_ENABLED = config('WTF_CSRF_ENABLED', cast=bool)
-    WTF_CSRF_TIME_LIMIT = None
-
-
-class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'db.sqlite3')
-    DEBUG = config('DEVELOPMENT_DEBUG', cast=bool)
-    SQLALCHEMY_ECHO = config('DEVELOPMENT_SQLALCHEMY_ECHO', cast=bool)
-    SESSION_COOKIE_SECURE = config('DEVELOPMENT_SESSION_COOKIE_SECURE', cast=bool)
-    PERMANENT_SESSION_LIFETIME = timedelta(hours=1.5)
-
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = config('PRODUCTION_DATABASE_URI')
-    DEBUG = config('PRODUCTION_DEBUG', cast=bool)
-    SQLALCHEMY_ECHO = config('PRODUCTION_SQLALCHEMY_ECHO', cast=bool)
-    SESSION_COOKIE_SECURE = config('PRODUCTION_SESSION_COOKIE_SECURE', cast=bool)
-    PERMANENT_SESSION_LIFETIME = timedelta(hours=1.5)
-
-
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = config('TEST_DATABASE_URI')
-    SQLALCHEMY_ECHO = config('TEST_SQLALCHEMY_ECHO', cast=bool)
-    TESTING = config('TESTING', cast=bool)
