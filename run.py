e4046cbc279fcf32d3bfc2539dcd49818888a47e
mugambi12 <silasmungiria16@gmail.com>
2024-06-20 00:15:36 +0300

    chore: Refactor file structure and imports for core backend modules

---
+++ run.py
@@ -0,0 +1,23 @@
+# File: run.py
+
+# Run in development mode using flask server
+from core.backend import create_app, db
+
+app = create_app()
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(port=app.config['PORT'], host=app.config['HOST'])
+
+#from core.backend import create_app, db
+#from waitress import serve
+#
+#app = create_app()
+#
+#with app.app_context():
+#    db.create_all()
+#
+#if __name__ == '__main__':
+#    serve(app, port=app.config['PORT'], host=app.config['HOST'])
