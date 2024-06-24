0e689f036dbea0c805bd43a246933030ece26b2a
mugambi12 <silasmungiria16@gmail.com>
2024-06-25 00:34:13 +0300

    refactor: Update dependencies and add lazy loading for images
    This commit updates the dependencies in the client/package.json file, adding "@react-pdf/renderer", "jspdf", and "lodash" to the list. Additionally, lazy loading for images is implemented to improve page load performance. This change enhances the functionality and performance of the application.
    
    Affected files:
    - client/package.json
    - client/src/components/Invoices/InvoiceManagement.css
    - client/src/components/Invoices/InvoiceTable.jsx

--- client/src/components/Invoices/InvoiceTable.jsx
+++ client/src/components/Invoices/InvoiceTable.jsx
@@ -1,7 +1,9 @@
 // InvoiceTable.js
+
 import React, { useEffect } from "react";
 import DataTable from "datatables.net-dt";
 import "datatables.net-responsive-dt";
+import jsPDF from "jspdf";
 
 const InvoiceTable = ({
   data,
@@ -20,20 +22,97 @@ const InvoiceTable = ({
     };
   }, [data]);
 
+  const handleDownloadPDF = (invoice) => {
+    const pdf = new jsPDF();
+
+    // Set styles for PDF content
+    pdf.setFont("helvetica");
+    pdf.setFontSize(12);
+    pdf.setTextColor(33, 33, 33);
+
+    // Add header
+    pdf.setTextColor(255, 0, 0);
+    pdf.setFontSize(18);
+    pdf.text("Invoice Details", 15, 15, { align: "left" });
+    pdf.setFontSize(16);
+    pdf.text(`${invoice.company_name}`, 195, 15, {
+      align: "right",
+    });
+    pdf.line(15, 20, 195, 20);
+
+    // Add invoice details
+    pdf.setTextColor(0, 0, 0);
+    pdf.setFontSize(12);
+    pdf.text(`Company Name: ${invoice.company_name}`, 15, 30);
+    pdf.text(`Full Name: ${invoice.full_name}`, 15, 40);
+    pdf.text(`House Section: ${invoice.house_section}`, 15, 50);
+    pdf.text(`House Number: ${invoice.house_number}`, 15, 60);
+    pdf.text(`Previous Reading: ${invoice.previous_reading}`, 15, 70);
+    pdf.text(`Current Reading: ${invoice.current_reading}`, 15, 80);
+    pdf.text(
+      `Consumption: ${invoice.consumption} ${
+        invoice.consumption !== 1 ? "Units" : "Unit"
+      }`,
+      15,
+      90
+    );
+    pdf.text(
+      `Unit Price: KES ${invoice.unit_price.toLocaleString()}`,
+      150,
+      100
+    );
+    pdf.text(
+      `Service Fee: KES ${invoice.service_fee.toLocaleString()}`,
+      150,
+      110
+    );
+    pdf.text(
+      `Total Amount: KES ${invoice.total_amount.toLocaleString()}`,
+      150,
+      120
+    );
+    pdf.text(
+      `Paid Amount: KES ${invoice.paid_amount.toLocaleString()}`,
+      150,
+      130
+    );
+    pdf.text(`Balance: KES ${invoice.balance.toLocaleString()}`, 150, 140);
+    pdf.text(`Payment Status: ${invoice.payment_status_text}`, 150, 150);
+    pdf.text(
+      `Date: ${new Date(invoice.created_at).toLocaleDateString()}`,
+      150,
+      160
+    );
+
+    // Add footer
+    pdf.setTextColor(100);
+    pdf.setFontSize(10);
+    pdf.text(`Invoice ID: ${invoice._id}`, 105, 290, { align: "center" });
+
+    // Get the current date
+    const currentDate = new Date();
+    const formattedDate = `${currentDate.getDate()}-${
+      currentDate.getMonth() + 1
+    }-${currentDate.getFullYear()}`;
+
+    // Save PDF
+    pdf.save(`${formattedDate}_invoice_${invoice._id}.pdf`);
+  };
+
   return (
     <div className="records-table-container">
       <table id="invoiceTable" className="records-invoice-table display nowrap">
         <thead>
           <tr>
             <th>Status</th>
-            {/*<th>User ID</th>*/}
             <th>Date</th>
             <th>Full Name</th>
             <th>Consumption</th>
             <th>Amount</th>
             <th>Balance</th>
-            <th>Action</th>
+            <th>Payment</th>
             <th>View</th>
+            <th>Download</th>
             <th>Delete</th>
           </tr>
         </thead>
@@ -45,7 +124,6 @@ const InvoiceTable = ({
                   className={`records-status ${invoice.payment_status}-bg`}
                 ></div>
               </td>
-              {/*<td>{invoice.user_id}</td>*/}
               <td>
                 <div className="records-date-info">
                   <span className="records-month">
@@ -99,6 +177,14 @@ const InvoiceTable = ({
                   onClick={() => openViewModal(invoice)}
                 >
                   visibility
+                </span>
+              </td>
+              <td>
+                <span
+                  className="material-symbols-rounded download"
+                  onClick={() => handleDownloadPDF(invoice)}
+                >
+                  download
                 </span>
               </td>
               <td>
